d472287edce0a2137cae2b46f74ef8f6f006c9b1
aleksej <xfrbt1@gmail.com>
2024-02-27 10:26:07 +0300

    analyzer

--- src/game_state/game_state.py
+++ src/game_state/game_state.py
@@ -27,22 +27,4 @@ class GameState:
             if value == max_value
         )
 
-        if count_max_values > 1:
-            """counting winners and make compare between same cards combinations players"""
-            """for non sequence combinations make compare of values on hands players card O(n^2)"""
-            """for all same combinations with a sequence, compare high card that sequence contains"""
-            self.draws += 1
-            """make pool of players with same combinations"""
-            """check if combination is a sequence or flush"""
-            """make this sequence for all players and compare sequential last card"""
-            """if this cards from players hand -> this player winner"""
-            players_same_result = [
-                k for k, v in self.players_combination_mapping.items() if v == max_value
-            ]
-            # print(players_same_result)
-            # print(self.players_combination_mapping)
-            # self.table.print_sate()
 
-        elif count_max_values == 1:
-            """1 person win, save data to database"""
-            ...
--- src/main.py
+++ src/main.py
@@ -1,31 +1,24 @@
-import cProfile
 import datetime
-import os
-import sys
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.insert(0, parent_dir)
-
 from src.game_state.game_state import GameState
 
 
 def emulate_not_finish_version(n, pl_n):
     game_state = GameState(pl_n)
-
     start = datetime.datetime.now()
+
     for i in range(n):
         game_state.update()
         game_state.analyze()
+
     end = datetime.datetime.now()
     draws = game_state.draws
-
     time = end - start
+
     print(f"|players: {pl_n}|games: {n}|same comb: {draws}|time: {time}|")
 
 
 def main(pl_n):
-    for n in [100000]:
+    for n in [100, 1000, 10000]:
         print("_______________________________________________________________")
         emulate_not_finish_version(n, pl_n)
 
